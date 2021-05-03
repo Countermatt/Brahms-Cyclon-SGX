085a8913bfef6474461d2ffe395430aba4275cae
Countermatt <matthieu.pgo@orange.fr>
2021-05-03 15:27:12 +0200

    init

---
+++ src/enclave/enclave.hpp
@@ -0,0 +1,52 @@
+#ifndef ENCLAVE_HPP_
+#define ENCLAVE_HPP_
+
+#include <vector>
+#include <string>
+
+
+#ifdef NATIVE
+#ifndef UAPP
+
+extern "C" {
+
+int ecall_Pull_Request(char** req,
+                   unsigned int* reqSize,
+                   char** destIP,
+                   unsigned int* destIPSize,
+                   Data* data);
+
+int ecall_Push_Request(char** req,
+                   unsigned int* reqSize,
+                   char** destIP,
+                   unsigned int* destIPSize,
+                   Data* data);
+
+int ecall_Pull_Reply(char** ans,
+                 unsigned int* ansSize,
+                 Data* data);
+
+int ecall_Pull_Receive_Reply(std::string  ans, Data* data);
+
+int ecall_Push_Receive_Request(std::string ans, Data* data);
+
+  // - Merge View
+void ecall_MergeView(int aL1, int bL2, int gL3, Data* data);
+
+int ecall_Ping(char** req,
+           unsigned int* reqSize,
+           char** destIP,
+           unsigned int* destIPSize,
+           Data* data);
+
+int ecall_Pong(char** ans,
+           unsigned int* ansSize,
+           Data* data);
+
+
+}
+#endif
+#endif
+
+
+#endif
